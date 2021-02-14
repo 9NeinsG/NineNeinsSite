aee56f22d40a6116927ec9369690141bb68affa1
GreyingError <44002584+9NeinsG@users.noreply.github.com>
2021-02-14 01:04:11 -0500

    Create var.js

---
+++ js/contrib/var.js
@@ -0,0 +1,6 @@
+var ip_country = "US";
+var cryptopayment_spending_limit_reached = "False";
+var cryptopayment_min_tokens = 100;
+var cryptopayment_max_tokens = 20000;
+var cryptopayment_btc_rate = 0.000021173076694470000000;
+var purchase_tokens_url = "/fashshop/powerlevel/";
