8e92ff9bf1ef87c575d727c841699710d5f9dd30
GreyingError <44002584+9NeinsG@users.noreply.github.com>
2021-02-26 21:26:01 -0500

    Create HideAdd.js

---
+++ js/Crypto/js/HideAdd.js
@@ -0,0 +1,64 @@
+//Hidden Address
+
+function copyContentsdc() {
+var $temp = $("<input>");
+var content = $('#doge_coin').text();
+
+$("body").append($temp);
+$temp.val(content).select();
+document.execCommand("copy");
+$temp.remove();
+
+}
+
+function copyContentsmc() {
+  var $temp = $("<input>");
+  var content = $('#monero_coin').text();
+
+    $("body").append($temp);
+    $temp.val(content).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+function copyContentsec() {
+  var $temp = $("<input>");
+  var content = $('#eth_coin').text();
+
+    $("body").append($temp);
+    $temp.val(content).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+function copyContentsbc() {
+  var $temp = $("<input>");
+  var content = $('#btc_coin').text();
+
+    $("body").append($temp);
+    $temp.val(content).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+//PopUp Script
+    
+function myFunctiond() {
+  var popup = document.getElementById("Popup-doge");
+  popup.classList.toggle("show");
+}
+
+function myFunctionm() {
+  var popup = document.getElementById("Popup-monero");
+  popup.classList.toggle("show");
+}
+
+function myFunctione() {
+  var popup = document.getElementById("Popup-eth");
+  popup.classList.toggle("show");
+}
+
+function myFunctionb() {
+  var popup = document.getElementById("Popup-btc");
+  popup.classList.toggle("show");
+}
